f11597149ea0ba99c221fac27a94286522df68d0
pixelatedpic <mohamedafzal@gmail.com>
2015-04-27 17:26:24 +0500

    Update test.lua

--- test.lua
+++ test.lua
@@ -44,3 +44,39 @@ srv:listen(80,function(conn)
   
     end)
 end)
+------------------------------------------------------------------
+wifi.setmode(wifi.SOFTAP)
+wifi.ap.config({ssid="xxx",pwd="welcome1234"})
+gpio.mode(0, gpio.OUTPUT)
+gpio.mode(1, gpio.OUTPUT)
+srv=net.createServer(net.TCP) 
+srv:listen(80,function(conn) 
+    conn:on("receive", function(client,request)
+        local buf = "";
+        local _, _, method, path, vars = string.find(request, "([A-Z]+) (.+)?(.+) HTTP");
+        if(method == nil)then 
+            _, _, method, path = string.find(request, "([A-Z]+) (.+) HTTP"); 
+        end
+        local _GET = {}
+        if (vars ~= nil)then 
+            for k, v in string.gmatch(vars, "(%w+)=(%w+)&*") do 
+                --_GET[k] = v 
+                print(k)
+                print(v)
+                
+                if (k=="CH1" and v=="ON") then
+                    gpio.write(0, gpio.HIGH)
+                elseif(k=="CH1" and v=="OFF") then
+                    gpio.write(0, gpio.LOW)
+                elseif(k=="CH2" and v=="OFF") then
+                    gpio.write(1, gpio.HIGH)
+                elseif(k=="CH2" and v=="OFF") then
+                    gpio.write(1, gpio.LOW) 
+                end   
+                
+                
+            end 
+        end
+  
+    end)
+end)
